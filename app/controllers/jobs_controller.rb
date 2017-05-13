7d7dbd14131e4f37ee2115dfaf8e83e1bd5da0cf
Tacuma Bell <takumab@gmail.com>
2017-05-13 12:38:48 +0100

    Repostion search form and add crud actions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,28 @@
 class JobsController < ApplicationController
-  def index
+
+  def home
   end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+    
 end
